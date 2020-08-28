dd71831088a342a721924fb6e6e7f136559ac898
Naptwen <47798805+Naptwen@users.noreply.github.com>
2020-08-28 08:44:03 -0700

    Delete TNEURON_METHODS.h

--- OLD/TNEURON_METHODS.h
+++
@@ -1,8 +0,0 @@
-#ifndef DLL_IFACE
-#ifdef DLL_IFACE_EXPORT
-#define DLL_IFACE _declspec( dllexport )
-#else  // !DLL_IFACE_EXPORT
-#define DLL_IFACE _declspec( dllimport )
-#endif // !DLL_IFACE_EXPORT
-#endif // !DLL_IFACE
-#include "TNEURON_METHODS.hpp"
